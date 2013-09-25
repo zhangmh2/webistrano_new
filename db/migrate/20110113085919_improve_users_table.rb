b0e0b49b8ec172d35cec67f62cda8d652a61d737
kazuhisa <ak.hisashi@gmail.com>
2013-09-25 09:59:50 +0900

    fixed table schema.
    Don't change admin columns type.

--- db/migrate/20110113085919_improve_users_table.rb
+++ db/migrate/20110113085919_improve_users_table.rb
@@ -3,7 +3,6 @@ class ImproveUsersTable < ActiveRecord::Migration
     change_table :users do |t|
       t.remove_index :disabled
       t.rename :disabled, :disabled_at
-      #t.change :admin, :boolean, :default => false
       t.index :disabled_at
     end
     remove_column :users, :admin
@@ -14,7 +13,6 @@ class ImproveUsersTable < ActiveRecord::Migration
     change_table :users do |t|
       t.remove_index :disabled_at
       t.rename :disabled_at, :disabled
-      #t.change :admin, :integer, :default => 0
       t.index :disabled
     end
     remove_column :users, :admin
